12f85f6de50ce2123480e9f00c656cfea722a3df
hectorleondev <payorayo@gmail.com>
2023-10-21 18:52:36 -0700

    Add connection to other db

--- database.py
+++ database.py
@@ -3,16 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLite
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 # # PostgresSQL
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test@localhost/TodoApplicationDatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Mysql
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
